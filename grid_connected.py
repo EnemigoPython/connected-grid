df7dace9efd8cf1a0f585c7506627545c828d643
Basil Eagle <basileagle@gmail.com>
2024-02-16 18:32:44 +0000

    Add files via upload

---
+++ grid_connected.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+def random_binary_array(x: int, y: int):
+    return np.random.randint(2, size=(x, y))
+
+def grid_is_connected(arr: np.array):
+    # initialise with all vals that are 1 in the first row
+    connected_above = set(e for e, el in enumerate(arr[0]) if el == 1)
+    for row in arr[1:]:
+        new_connected = set()
+        for e in connected_above:
+            # check each val connected to 1s in prev row
+            if row[e] == 0:
+                continue
+            new_connected.add(e)
+            # if there is a connection, check left and right
+            for d in (-1, 1):
+                curr = e + d
+                while curr >= 0 and curr < len(row):
+                    # in each direction stop when no direct path
+                    if row[curr] == 0:
+                        break
+                    new_connected.add(curr)
+                    curr += d
+        if not new_connected:
+            return False
+        connected_above = new_connected
+    return True
+
+
+
+def main():
+    connected_arrs = {}
+    for size in range(3, 10):
+        connected_arrs[size] = []
+        for _ in range(20):
+            arr = random_binary_array(size, size)
+            if grid_is_connected(arr):
+                connected_arrs[size].append(arr)
+
+    for k, v in connected_arrs.items():
+        print(f"\nConnected arrays of size {k}:")
+        for i in v:
+            print(f"\n{i}")
+
+
+
+if __name__ == '__main__':
+    main()
